7cf774a0520ef8dc4903725760def6cef413aa40
Plamen Terziev <pterziev@datexcorp.com>
2023-06-02 01:26:57 +0300

    Updated code

--- frontend-react/src/helpers/auth_helper.js
+++ frontend-react/src/helpers/auth_helper.js
@@ -4,7 +4,7 @@ const settings = {
   authority: "https://api3.hireya.org/realms/admin_console_realm",
   client_id: "admin_console_client",
   client_secret: "auD2uP47zVYsegYH5eHoa1IPwuEytoBx",
-  redirect_uri: window.location.origin,
+  redirect_uri: "https://react2-liard.vercel.app/signin-callback.html",
   response_type: 'code',
   scope: "openid",
 };
