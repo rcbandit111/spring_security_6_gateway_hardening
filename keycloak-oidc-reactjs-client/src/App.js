0b66600984e9ec73135175ee1083f8748ab4aade
Plamen Terziev <pterziev@datexcorp.com>
2023-06-02 20:34:47 +0300

    Updated code

--- keycloak-oidc-reactjs-client/src/App.js
+++ keycloak-oidc-reactjs-client/src/App.js
@@ -1,72 +1,59 @@
 import React from "react";
 import {
-    Route,
-    Routes,
-    Outlet,
-    Navigate,
-    useLocation,
-    BrowserRouter as Router,
+  Route,
+  Routes,
+  Outlet,
+  Navigate,
+  useLocation,
+  BrowserRouter as Router,
 } from "react-router-dom";
 import { getCurrentUser } from "./model/Functions";
 import Home from "./component/Home";
 import AuthorisedPage from "./component/AuthorisedPage";
 
-
-
 const PrivateRoute = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-
-
-    return getCurrentUser() ? (
-        <Outlet />
-    ) : (
-        <Navigate
-            to={{
-                pathname: "/",
-                state: { from: location },
-            }}
-        />
-    );
+  return getCurrentUser() ? (
+    <Outlet />
+  ) : (
+    <Navigate
+      to={{
+        pathname: "/",
+        state: { from: location },
+      }}
+    />
+  );
 };
-
-
 
 const NonPrivateRoute = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-
-
-    return getCurrentUser() ? (
-        <Navigate
-            to={{
-                pathname: "/auth",
-                state: { from: location },
-            }}
-        />
-    ) : (
-        <Outlet />
-    );
+  return getCurrentUser() ? (
+    <Navigate
+      to={{
+        pathname: "/auth",
+        state: { from: location },
+      }}
+    />
+  ) : (
+    <Outlet />
+  );
 };
 
-
-
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<NonPrivateRoute />}>
-                    <Route index element={<Home />} />
-                </Route>
-                <Route path="/auth" element={<PrivateRoute />}>
-                    <Route index element={<AuthorisedPage />} />
-                </Route>
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<NonPrivateRoute />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/auth" element={<PrivateRoute />}>
+          <Route index element={<AuthorisedPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-
-
 export default App;
-
--- keycloak-oidc-reactjs-client/src/component/AuthorisedPage.js
+++ keycloak-oidc-reactjs-client/src/component/AuthorisedPage.js
@@ -1,26 +1,47 @@
-import React, { Component } from 'react';
-import { CookieName } from '../model/Config';
-import { GetCookieValue } from '../model/Functions';
-import axios from 'axios';
+import React, { Component } from "react";
+import { CookieName } from "../model/Config";
+import { GetCookieValue } from "../model/Functions";
+import axios from "axios";
 
-class AuthorisedPage extends Component{
-    constructor(){
-        super();
-        this.state = {
-            username: GetCookieValue(CookieName)
-        }
-    }
-    render(){
-        let { username } = this.state;
-        return(
-            <div className="mdWrapper">
-                <img src="./image/logo.png" alt="Keycloak OIDC" />
-                <h3>Keycloak - OIDC</h3>
-                <h4>React JS - Implicit flow authentication</h4>
-                {username ? (<p>Welcome <strong>{username}</strong></p>) : ""}
-            </div>
-        )
-    }
+class AuthorisedPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: GetCookieValue(CookieName),
+      data: [],
+    };
+  }
+  componentDidMount() {
+    const parts = this.state.username.split(" ");
+    const token = parts[parts.length - 1];
+
+    axios
+      .get("/api/microservice/dashboard/test", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
+  render() {
+    let { username } = this.state;
+    return (
+      <div className="mdWrapper">
+        <img src="./image/logo.png" alt="Keycloak OIDC" />
+        <h3>Keycloak - OIDC</h3>
+        <h4>React JS - Implicit flow authentication</h4>
+        {username ? (
+          <p>
+            Welcome <strong>{username}</strong>
+          </p>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
 }
 
 export default AuthorisedPage;
