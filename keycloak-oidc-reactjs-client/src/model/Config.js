f03a8b8fe65f921dce77eb0287464446206d3463
Plamen Terziev <pterziev@datexcorp.com>
2023-06-12 14:46:39 +0300

    Updated code

--- keycloak-oidc-reactjs-client/src/model/Config.js
+++ keycloak-oidc-reactjs-client/src/model/Config.js
@@ -2,5 +2,5 @@ export const CookieName = "Keycloak_Auth";
 export const Config = {
     "url": "https://api3.hireya.org",
     "realm": "admin_console_realm",
-    "clientId": "admin_console_client"
+    "clientId": "react_client"
 }
